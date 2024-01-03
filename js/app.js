26623604ed991167f7e3c7c6a3e6ca3a6436e5ab
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-01-03 21:52:28 +0900

    デコボコの斜面を作成する

--- js/app.js
+++ js/app.js
@@ -20,6 +20,10 @@ var noise = x => {
 function loop() {
     ctx.fillStyle = "#19f";
     ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "black";
+
+    ctx.beginPath();
     requestAnimationFrame(loop);
 }
 

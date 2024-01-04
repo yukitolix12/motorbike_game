5779104012d517a52390e630cfb2edaad0dd0bc0
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-01-04 17:12:16 +0900

    道のデコボコを作るよう数値を設定する

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ while (perm.length < 255) {
 var lerp = (a, b, t) => a + (b - a) * t;
 
 var noise = x => {
-    x = x % 255;
+    x = x * 0.01 % 255;
     return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
 }
 

cc240ea9727967864fec73ff059ca603172abccf
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-01-05 13:52:35 +0900

    数値を調整、変数を新たに加え、山の表面にplayerが密着するようにする

--- js/app.js
+++ js/app.js
@@ -31,9 +31,10 @@ var player = new function() {
     this.draw = function() {
         var p1 = c.height - noise(t + this.x) * 0.25;
 
-        if(p1 > this.y) {
+        if(p1 - 15 > this.y) {
             this.ySpeed -= 0.1;
         } else {
+            this.y = p1;
             this.ySpeed = 0;
         }
 
